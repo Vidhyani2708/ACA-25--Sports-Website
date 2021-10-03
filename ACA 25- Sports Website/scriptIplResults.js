666b991291257bddd49f725d46038196fcd9c4de
Vidhyani2708 <90394503+Vidhyani2708@users.noreply.github.com>
2021-10-03 16:06:14 +0530

    Add files via upload

---
+++ ACA 25- Sports Website/scriptIplResults.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCbk8531cxl9AmZjrveOJWhaxQ6HQpKXD8",
+  authDomain: "ipl-results-b3838.firebaseapp.com",
+  databaseURL: "https://ipl-results-b3838-default-rtdb.firebaseio.com",
+  projectId: "ipl-results-b3838",
+  storageBucket: "ipl-results-b3838.appspot.com",
+  messagingSenderId: "846907265591",
+  appId: "1:846907265591:web:349392d0f4005feeeae5b2"
+};
+
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
